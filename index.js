0478d7f68144268af79f205336a5d6da9b370f16
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-10-05 03:17:10 +0200

    added more package to node modules

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import {randomSupervillain} from 'supervillains';
+import { randomSuperhero } from 'superheroes';
 
-console.log(randomSupervillain());
+console.log(`"${randomSupervillain()}" is a supervillain.`);
 //=> 'Mud Pack'
+
+// display random superheros
+console.log(`"${randomSuperhero()}" is a superhero.`);
