6ca3d29b8d7450fe5ab65070502a2ff0997f0e39
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-10-22 06:54:46 +0200

    added discriptive comment on the informative warning the appears while running the code in the terminal || command line

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ console.log(`"${randomSupervillain()}" is a supervillain.`);
 
 // display random superheros
 console.log(`"${randomSuperhero()}" is a superhero.`);
+
+
+/*
+    What you'll notice after running 'node index.js' || 'nodemon index.js' , you will get a warning indicating that you have module that your curring code module depend on and could be changed at any time by their various authors
+*/
