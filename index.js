da23c71dd73b0cea30ef74d8bec2e2bb46c74d38
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-10-28 06:29:29 +0200

    format comment well for developer readability

--- index.js
+++ index.js
@@ -9,5 +9,10 @@ console.log(`"${randomSuperhero()}" is a superhero.`);
 
 
 /*
-    What you'll notice after running 'node index.js' || 'nodemon index.js' , you will get a warning indicating that you have module that your curring code module depend on and could be changed at any time by their various authors
+    What you'll notice after running
+    'node index.js' || 'nodemon index.js', 
+    you will get a warning indicating that
+    you have module that your current code
+    module depend on and could be changed 
+    at any time by their various authors.
 */
